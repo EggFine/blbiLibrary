28618c8870783c39642beee4eff9f3d9d219b8a1
EggFine <1079742633@qq.com>
2024-07-19 19:03:28 +0800

    first commit

---
+++ src/main/java/org/blbilink/blbiLibrary/BlbiLibrary.java
@@ -0,0 +1,17 @@
+package org.blbilink.blbiLibrary;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlbiLibrary extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

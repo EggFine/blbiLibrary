7903257a5da46d96072d336d184f81fcb8e4193b
EggFine <1079742633@qq.com>
2024-07-23 05:14:54 +0800

    update

--- src/main/java/org/blbilink/blbiLibrary/utils/PapiUtil.java
+++ src/main/java/org/blbilink/blbiLibrary/utils/PapiUtil.java
@@ -2,32 +2,42 @@ package org.blbilink.blbiLibrary.utils;
 
 import me.clip.placeholderapi.PlaceholderAPI;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.blbilink.blbiLibrary.BlbiLibrary;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 public class PapiUtil {
+    private BlbiLibrary blbiLibrary = BlbiLibrary.blbiLibrary;
+    private Plugin plugin;
+
+    public PapiUtil(Plugin plugin) {
+        this.plugin = plugin;
+    }
 
     /**
      * 解析包含 PAPI 变量的字符串
+     *
      * @param player 玩家对象
-     * @param text 包含 PAPI 变量的文本
+     * @param text   包含 PAPI 变量的文本
      * @return 解析后的文本
      */
-    public static String setPlaceholders(Player player, String text) {
+    public String setPlaceholders(Player player, String text) {
         return PlaceholderAPI.setPlaceholders(player, text);
     }
 
     /**
      * 注册自定义的 PAPI 扩展
+     *
      * @param expansion 自定义的 PlaceholderExpansion 实现
      */
-    public static void registerExpansion(PlaceholderExpansion expansion) {
+    public void registerExpansion(PlaceholderExpansion expansion) {
         expansion.register();
     }
 
     /**
      * 创建一个简单的 PAPI 扩展
      */
-    public static abstract class SimpleExpansion extends PlaceholderExpansion {
+    public abstract class SimpleExpansion extends PlaceholderExpansion {
         private final String identifier;
         private final String author;
         private final String version;
--- src/main/java/org/blbilink/blbiLibrary/utils/PluginUtil.java
+++ src/main/java/org/blbilink/blbiLibrary/utils/PluginUtil.java
@@ -1,16 +1,35 @@
 package org.blbilink.blbiLibrary.utils;
 
+import org.blbilink.blbiLibrary.BlbiLibrary;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PluginUtil {
+    private final BlbiLibrary blbiLibrary = BlbiLibrary.blbiLibrary;
+    private final Plugin plugin;
+    public List<String> plugins = new ArrayList<>();
+
+    public PluginUtil(Plugin plugin) {
+        this.plugin = plugin;
+    }
+
     /**
      * 检查插件是否已经加载（多数用于软前置判断）
+     *
      * @param pluginName 要检查是否加载的插件名
      * @return 如果返回true则表示已加载，否则反之
      */
-    public static boolean isPluginLoaded(String pluginName) {
+    public boolean isPluginLoaded(String pluginName) {
         Plugin plugin = Bukkit.getPluginManager().getPlugin(pluginName);
-        return plugin != null && plugin.isEnabled();
+        if (plugin != null && plugin.isEnabled()) {
+            plugins.add(pluginName);
+            blbiLibrary.getLogger().info(AnsiColor.AQUA + "[√] 已检测到 " + pluginName + " 插件， [" + plugin.getName() + "] 插件使用其作为软依赖" + AnsiColor.RESET);
+            plugin.getLogger().info(AnsiColor.AQUA + "[√] 已检测到 " + pluginName + " 前置，正在进行 Hook" + AnsiColor.RESET);
+            return true;
+        }
+        return false;
     }
 }
